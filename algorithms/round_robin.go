64753bc5b54b5d84a82e6e436a8596cc2a641b77
stfbx <coconut.tadpole@proton.me>
2025-12-28 00:52:21 -0300

    Use atomic int32 to mitigate race conditions on RoundRobin counter

--- algorithms/round_robin.go
+++ algorithms/round_robin.go
@@ -1,15 +1,16 @@
 package algorithms
 
-import "net/http"
+import (
+	"net/http"
+	"sync/atomic"
+)
 
 type RoundRobin struct {
-	selected int
+	counter int32
 }
 
 func (m *RoundRobin) GetServer(server_list []string, request *http.Request) string {
-	server := server_list[m.selected]
-
-	m.selected = (m.selected + 1) % len(server_list)
-
-	return server
+	next := atomic.AddInt32(&m.counter, 1)
+	index := int(next-1) % len(server_list)
+	return server_list[index]
 }
